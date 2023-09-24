f9fe3db13a905659688330c74d074924e5a26e49
Kooper1234 <142433164+Kooper1234@users.noreply.github.com>
2023-09-24 10:37:08 -0400

    Update main.py

--- main.py
+++ main.py
@@ -14,30 +14,35 @@ def generate_essay(prompt, max_tokens):
 # Streamlit Interface
 st.title('College Essay Generator')
 
-st.write('Let\'s gather some information to create a personalized college essay for you.')
+# Step 1: Get the general essay prompt from the user
+essay_prompt = st.text_input('Please enter the general essay prompt or theme:')
 
-# Questions for the user
-name = st.text_input('What\'s your name?')
-hobby = st.text_input('Describe a hobby or activity you are passionate about:')
-achievement = st.text_input('Mention a significant achievement or experience you had:')
-lesson_learned = st.text_input('Describe a lesson you learned from a challenging experience:')
-future_goals = st.text_input('What are your future goals or aspirations?')
-word_count = st.number_input('How many words should the essay be?', min_value=50, max_value=1000, value=500, step=50)
+if essay_prompt:
+    # Step 2: Break down the prompt into specifics
+    st.write('Help us understand more about your prompt for a personalized essay.')
+    
+    detail_1 = st.text_input(f"Elaborate on a specific point related to '{essay_prompt}' (e.g., a personal experience, a particular aspect, etc.):")
+    detail_2 = st.text_input(f"Describe any additional aspect or perspective you'd like to cover related to '{essay_prompt}':")
+    detail_3 = st.text_input(f"Is there a takeaway or lesson you want to be highlighted in relation to '{essay_prompt}'?")
 
-# Convert word count to approximate tokens. This is a rough estimate.
-tokens = int(word_count * 1.5)  # Assuming each word translates to around 1.5 tokens
+    # Step 3: Ask for word count
+    word_count = st.number_input('How many words should the essay be?', min_value=50, max_value=1000, value=500, step=50)
+    tokens = int(word_count * 1.5)  # Rough token estimate
 
-# Generating the prompt based on user's input
-if all([name, hobby, achievement, lesson_learned, future_goals]):
-    full_prompt = f"Write a college essay that incorporates the following details: \
-                    \n- A student named {name} who is passionate about {hobby}. \
-                    \n- They had a significant experience where {achievement}. \
-                    \n- From a challenging situation, they learned that {lesson_learned}. \
-                    \n- In the future, they aspire to {future_goals}. \
-                    \n\nBegin the essay:\n"
+    # Step 4: If all details are provided, generate the essay
+    if all([detail_1, detail_2, detail_3]):
+        full_prompt = f"Based on the theme '{essay_prompt}', considering the following specifics: \
+                        \n1. {detail_1} \
+                        \n2. {detail_2} \
+                        \n3. {detail_3} \
+                        \n\nWrite an essay:\n"
 
-    # Fetch the generated essay
-    essay = generate_essay(full_prompt, tokens)
-    st.write(essay)
-else:
-    st.write('Please answer all the questions to generate your essay.')
+        essay = generate_essay(full_prompt, tokens)
+        st.write(essay)
+This approach will ask the user for their general essay prompt, then provide more input fields based on this prompt. Finally, once all details are filled in, the user can specify the word count and generate the essay.
+
+
+
+
+
+
