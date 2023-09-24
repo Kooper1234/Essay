36410e969a7acadb947cd90ba54ad7b3f4dde2e7
Kooper1234 <142433164+Kooper1234@users.noreply.github.com>
2023-09-24 10:45:53 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,17 @@ import streamlit as st
 import openai
 openai.api_key = st.secrets["OPENAI_API_KEY"]
 
+def generate_questions(prompt):
+    question_prompt = f"Given the essay topic '{prompt}', what are some specific questions that can help in generating a detailed and comprehensive essay?"
+    response = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=question_prompt,
+        temperature=0.5,
+        max_tokens=150  # Limiting tokens for generating questions
+    )
+    # Splitting the response to get individual questions
+    return [q.strip() for q in response.choices[0].text.split('\n') if q]
+
 def generate_essay(prompt, max_tokens):
     response = openai.Completion.create(
         engine="text-davinci-003",
@@ -14,29 +25,26 @@ def generate_essay(prompt, max_tokens):
 # Streamlit Interface
 st.title('College Essay Generator')
 
-# Step 1: Get the general essay prompt from the user
+# Step 1: Get the main essay prompt
 essay_prompt = st.text_input('Please enter the general essay prompt or theme:')
 
+details = []
 if essay_prompt:
-    # Step 2: Break down the prompt into specifics
-    st.write('Help us understand more about your prompt for a personalized essay.')
+    # Step 2: Use AI to generate specific questions based on the main prompt
+    questions = generate_questions(essay_prompt)
     
-    detail_1 = st.text_input(f"Elaborate on a specific point related to '{essay_prompt}' (e.g., a personal experience, a particular aspect, etc.):")
-    detail_2 = st.text_input(f"Describe any additional aspect or perspective you'd like to cover related to '{essay_prompt}':")
-    detail_3 = st.text_input(f"Is there a takeaway or lesson you want to be highlighted in relation to '{essay_prompt}'?")
+    for q in questions:
+        answer = st.text_input(q)
+        if answer:
+            details.append(answer)
 
     # Step 3: Ask for word count
     word_count = st.number_input('How many words should the essay be?', min_value=50, max_value=1000, value=500, step=50)
     tokens = int(word_count * 1.5)  # Rough token estimate
 
     # Step 4: If all details are provided, generate the essay
-    if all([detail_1, detail_2, detail_3]):
-        full_prompt = f"Based on the theme '{essay_prompt}', considering the following specifics: \
-                        \n1. {detail_1} \
-                        \n2. {detail_2} \
-                        \n3. {detail_3} \
-                        \n\nWrite an essay:\n"
-
+    if len(details) == len(questions):  # All questions answered
+        full_prompt = f"Based on the theme '{essay_prompt}', considering the following specifics: " + ' '.join(details) + "\n\nWrite an essay:\n"
         essay = generate_essay(full_prompt, tokens)
         st.write(essay)
 
